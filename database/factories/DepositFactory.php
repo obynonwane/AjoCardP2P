89d789d25218c651fbd9e1c3bf69669a452206db
OBINNA <biostechnologyng@gmail.com>
2019-09-20 10:12:58 -0700

    Created API Resouce Controller Migration and Models

---
+++ database/factories/DepositFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Deposit;
+use Faker\Generator as Faker;
+
+$factory->define(Deposit::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
